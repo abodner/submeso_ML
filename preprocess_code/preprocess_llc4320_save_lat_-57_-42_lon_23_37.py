9d9d7adcf700b437bd9a3c6b17b3c409f6f7c373
Abigail Bodner <ab10313@log-2.hpc.nyu.edu>
2022-10-04 11:22:17 -0400

    changed script to save full variables(not just sanpshots)

--- preprocess_code/preprocess_llc4320_save_lat_-57_-42_lon_23_37.py
+++ preprocess_code/preprocess_llc4320_save_lat_-57_-42_lon_23_37.py
@@ -14,15 +14,17 @@ import matplotlib.pyplot as plt
 import xesmf as xe
 import gsw_xarray as gsw
 import os
-
+from datetime import datetime
 
 CASE_NAME = 'lat_-57_-42_lon_23_37/'
 BASE = '/scratch/ab10313/submeso_ML_data/'
 PATH = BASE+CASE_NAME
 
 print('PREPROCESS CASE '+CASE_NAME)
+print(datetime.now())
 
 print('T,S,U,V,W FROM LLC4320')
+print(datetime.now())
 # load full temperature and salinity datasets
 ds_T_full = model.get_dataset(varnames=['Theta'], type='latlon')
 ds_S_full = model.get_dataset(varnames=['Salt'], type='latlon')
@@ -61,7 +63,7 @@ W = ds_W_full_interp.W.resample(time='24H').nearest(tolerance="1H").where(sel_ar
 del ds_T_full, ds_S_full, ds_UV_full_interp, ds_W_full_interp
 
 print('CALCULATE B')
-
+print(datetime.now())
 # calculate buoyancy from temp and salt, using the gsw package
 # pressure
 P = gsw.p_from_z(T.Z, T.YC)
@@ -88,6 +90,7 @@ del S, P, G, CT
 
 
 print('SAVE SNAPSHOT: B,U,V,W')
+print(datetime.now())
 # select middle 10 degree box for snapshot
 lon_mid = (lon_min + lon_max)/2
 lat_mid = (lat_min + lat_max)/2
@@ -126,27 +129,31 @@ def save_snapshot(PATH, var, var_save_name):
     del var_values
 
 #################
+'''
+save_snapshot(PATH,T.XC.isel(i=i_slice_10deg,j=j_slice_10deg)[0,:],'lon')
+save_snapshot(PATH,T.YC.isel(i=i_slice_10deg,j=j_slice_10deg)[:,0],'lat')
 
 save_snapshot(PATH,B.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'B')
 save_snapshot(PATH,U.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'U')
 save_snapshot(PATH,V.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'V')
 save_snapshot(PATH,W.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'W')
-
+'''
 print('SAVE SNAPSHOT: MLD')
+print(datetime.now())
 # sigma0 at 10m depth for reference
 sigma0_10m = sigma0.isel(k=6).broadcast_like(sigma0).chunk(chunks={'time': 1, 'k':1, 'j': sigma0.j.size, 'i': sigma0.i.size})
 
 delta_sigma = sigma0 - sigma0_10m
-
+'''
 # mixed layer depth
 MLD = sigma0.Z.broadcast_like(sigma0).where(delta_sigma<=0.03).min(dim="k",skipna=True).chunk(chunks={'time': 1, 'j': sigma0.j.size, 'i': sigma0.i.size}).rename('Mixed Layer Depth')
 
 save_snapshot(PATH,MLD.isel(time=0,i=i_slice_10deg,j=j_slice_10deg),'MLD')
 
 del MLD
-
+'''
 print('FILTER')
-
+print(datetime.now())
 # # Filtering: fixed factor scale 24
 
 gcm_filters.required_grid_vars(gcm_filters.GridType.IRREGULAR_WITH_LAND)
@@ -227,13 +234,14 @@ Um = filter_factor24.apply(U.where(wet_mask), dims=['i', 'j'])
 Vm = filter_factor24.apply(V.where(wet_mask), dims=['i', 'j'])
 Wm = filter_factor24.apply(W.where(wet_mask), dims=['i', 'j'])
 Bm = filter_factor24.apply(B.where(wet_mask), dims=['i', 'j'])
-
+'''
 print('SAVE SNAPSHOT: Bm,Um,Vm,Wm')
+print(datetime.now())
 save_snapshot(PATH,Bm.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Bm')
 save_snapshot(PATH,Um.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Um')
 save_snapshot(PATH,Vm.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Vm')
 save_snapshot(PATH,Wm.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Wm')
-
+'''
 # submesoscale field defined as the deviation between the full resolved field and mesoscale field
 
 Us = U - Um
@@ -242,13 +250,14 @@ Ws = W - Wm
 Bs = B - Bm
 
 del U, V, W, B
-
+'''
 print('SAVE SNAPSHOT: Bs,Us,Vs,Ws')
+print(datetime.now())
 save_snapshot(PATH,Bs.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Bs')
 save_snapshot(PATH,Us.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Us')
 save_snapshot(PATH,Vs.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Vs')
 save_snapshot(PATH,Ws.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Ws')
-
+'''
 # submesoscale buoyancy fluxes, with a mesoscale filter
 
 UsBs = filter_factor24.apply((Us * Bs).where(wet_mask), dims=['i', 'j'])
@@ -256,25 +265,28 @@ VsBs = filter_factor24.apply((Vs * Bs).where(wet_mask), dims=['i', 'j'])
 WsBs = filter_factor24.apply((Ws * Bs).where(wet_mask), dims=['i', 'j'])
 
 del Us, Vs, Ws, Bs
-
+'''
 print('SAVE SNAPSHOT: UsBs,VsBs,WsBs')
+print(datetime.now())
 save_snapshot(PATH,UsBs.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'UsBs')
 save_snapshot(PATH,VsBs.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'VsBs')
 save_snapshot(PATH,WsBs.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'WsBs')
 
-
+'''
 # mesoscale buoyancy gradient
 # need to temperarily rechunk for the z derivative
 Bm_x = Bm.differentiate("i")
 Bm_y = Bm.differentiate("j")
-Bm_z = Bm.chunk(chunks={"time":len(T.time), "k":len(T.k), "j":len(T.j), "i": len(T.i)}).differentiate("k").chunk(chunks={"time":1, "k":1, "j":len(T.j), "i": len(T.i)})
-
+Bm_z = Bm.chunk(chunks={"time":2, "k":2, "j":len(T.j), "i": len(T.i)}).differentiate("k").chunk(chunks={"time":1, "k":1, "j":len(T.j), "i": len(T.i)})
+'''
 print('SAVE SNAPSHOT: Bm_x,Bm_y,Bm_z')
+print(datetime.now())
 save_snapshot(PATH,Bm_x.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Bm_x')
 save_snapshot(PATH,Bm_y.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Bm_y')
 save_snapshot(PATH,Bm_z.isel(time=0,k=0,i=i_slice_10deg,j=j_slice_10deg),'Bm_z')
-
+'''
 print('COARSE-GRAIN')
+print(datetime.now())
 ### Coarse-grain to 1/4 degree
 # building regridder
 ds_out_qurter = xr.Dataset(
@@ -288,13 +300,23 @@ regridder_quarter = xe.Regridder(T.isel(time=0, k=0), ds_out_qurter, "bilinear",
 regridder_quarter
 
 
-
 # regrid and select middle 10 degree box
 lon_mid = (lon_min + lon_max)/2
 lat_mid = (lat_min + lat_max)/2
 
 lon_slice_10deg = slice(lon_mid-5,lon_mid+5)
 lat_slice_10deg = slice(lat_mid-5,lat_mid+5)
+
+# save 10deg lat lowres and lon lowres grids
+lat_lowres = np.arange(lat_min, lat_max, 0.25)
+lat_lowres = lat_lowres[np.where(np.logical_and(lat_lowres>=lat_mid-5,lat_lowres<=lat_mid+5))]
+
+lon_lowres = np.arange(lon_min, lon_max, 0.25)
+lon_lowres = lon_lowres[np.where(np.logical_and(lon_lowres>=lon_mid-5,lon_lowres<=lon_mid+5))]
+'''
+np.save(PATH+'snapshots/lat_lowres.npy',lon_lowres)
+np.save(PATH+'snapshots/lon_lowres.npy',lat_lowres)
+'''
 
 # meso velocities and buoyancy
 Um_lowres = regridder_quarter(Um).sel(lon=lon_slice_10deg,lat=lat_slice_10deg) 
@@ -313,64 +335,88 @@ VsBs_lowres = regridder_quarter(VsBs).sel(lon=lon_slice_10deg,lat=lat_slice_10de
 WsBs_lowres = regridder_quarter(WsBs).sel(lon=lon_slice_10deg,lat=lat_slice_10deg)  
 
 del Um, Vm, Wm, Bm, UsBs, VsBs, WsBs, Bm_x, Bm_y, Bm_z
-
+'''
 print('SAVE SNAPSHOT LOWRES: Bm,Um,Vm,Wm')
+print(datetime.now())
 save_snapshot(PATH,Bm_lowres.isel(time=0,k=0),'Bm_lowres')
 save_snapshot(PATH,Um_lowres.isel(time=0,k=0),'Um_lowres')
 save_snapshot(PATH,Vm_lowres.isel(time=0,k=0),'Vm_lowres')
 save_snapshot(PATH,Wm_lowres.isel(time=0,k=0),'Wm_lowres')
 
 print('SAVE SNAPSHOT LOWRES: UsBs,VsBs,WsBs')
+print(datetime.now())
 save_snapshot(PATH,UsBs_lowres.isel(time=0,k=0),'UsBs_lowres')
 save_snapshot(PATH,VsBs_lowres.isel(time=0,k=0),'VsBs_lowres')
 save_snapshot(PATH,WsBs_lowres.isel(time=0,k=0),'WsBs_lowres')
 
 print('SAVE SNAPSHOT LOWRES: Bm_x,Bm_y,Bm_z')
+print(datetime.now())
 save_snapshot(PATH,Bm_x_lowres.isel(time=0,k=0),'Bm_x_lowres')
 save_snapshot(PATH,Bm_y_lowres.isel(time=0,k=0),'Bm_y_lowres')
 save_snapshot(PATH,Bm_z_lowres.isel(time=0,k=0),'Bm_z_lowres')
-
+'''
 print('MLD AVERAGE')
+print(datetime.now())
 # meso velocities and buoyancy MLD average
 Um_lowres_MLD = Um_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('Um_lowres_MLD')
 Vm_lowres_MLD = Vm_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('Vm_lowres_MLD')
 Wm_lowres_MLD = Wm_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('Wm_lowres_MLD')
 Bm_lowres_MLD = Bm_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('Bm_lowres_MLD')
+del Um_lowres, Vm_lowres, Wm_lowres, Bm_lowres
 
 # submeso fluxes MLD average
 UsBs_lowres_MLD = UsBs_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('UsBs_lowres_MLD')
 VsBs_lowres_MLD = VsBs_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('VsBs_lowres_MLD')
 WsBs_lowres_MLD = WsBs_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('WsBs_lowres_MLD')
+del UsBs_lowres, VsBs_lowres, WsBs_lowres
 
 # meso gradients MLD average
 Bm_x_lowres_MLD = Bm_x_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('Bm_x_lowres_MLD')
 Bm_y_lowres_MLD = Bm_y_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('Bm_y_lowres_MLD')
 Bm_z_lowres_MLD = Bm_z_lowres.where(delta_sigma<=0.03).min(dim="k",skipna=True).rename('Bm_z_lowres_MLD')
-
-
-del Um_lowres, Vm_lowres, Wm_lowres, Bm_lowres
-del UsBs_lowres, VsBs_lowres, WsBs_lowres
 del Bm_x_lowres, Bm_y_lowres, Bm_z_lowres
 
+
+# SAVE NETCDF
+print('SAVE NETCDF: MLD AVG LOWRES')
+print('meso fields')
+Um_lowres_MLD.to_netcdf(PATH+'data/Um_lowres_MLD.nc',engine='h5netcdf')
+Vm_lowres_MLD.to_netcdf(PATH+'data/Vm_lowres_MLD.nc',engine='h5netcdf')
+Wm_lowres_MLD.to_netcdf(PATH+'data/Wm_lowres_MLD.nc',engine='h5netcdf')
+Bm_lowres_MLD.to_netcdf(PATH+'data/Bm_lowres_MLD.nc',engine='h5netcdf')
+
+print('submeso fluxes')
+UsBs_lowres_MLD.to_netcdf(PATH+'data/UsBs_lowres_MLD.nc',engine='h5netcdf')
+VsBs_lowres_MLD.to_netcdf(PATH+'data/VsBs_lowres_MLD.nc',engine='h5netcdf')
+WsBs_lowres_MLD.to_netcdf(PATH+'data/WsBs_lowres_MLD.nc',engine='h5netcdf')
+
+print('meso B grad')
+Bm_x_lowres_MLD.to_netcdf(PATH+'data/Bm_x_lowres_MLD.nc',engine='h5netcdf')
+Bm_y_lowres_MLD.to_netcdf(PATH+'data/Bm_y_lowres_MLD.nc',engine='h5netcdf')
+Bm_z_lowres_MLD.to_netcdf(PATH+'data/Bm_z_lowres_MLD.nc',engine='h5netcdf')
+'''
 print('SAVE SNAPSHOT MLD AVG LOWRES: Bm,Um,Vm,Wm')
+print(datetime.now())
 save_snapshot(PATH,Bm_lowres_MLD.isel(time=0),'Bm_lowres_MLD')
 save_snapshot(PATH,Um_lowres_MLD.isel(time=0),'Um_lowres_MLD')
 save_snapshot(PATH,Vm_lowres_MLD.isel(time=0),'Vm_lowres_MLD')
 save_snapshot(PATH,Wm_lowres_MLD.isel(time=0),'Wm_lowres_MLD')
 
 print('SAVE SNAPSHOT MLD AVG LOWRES: UsBs,VsBs,WsBs')
+print(datetime.now())
 save_snapshot(PATH,UsBs_lowres_MLD.isel(time=0),'UsBs_lowres_MLD')
 save_snapshot(PATH,VsBs_lowres_MLD.isel(time=0),'VsBs_lowres_MLD')
 save_snapshot(PATH,WsBs_lowres_MLD.isel(time=0),'WsBs_lowres_MLD')
 
 print('SAVE SNAPSHOT MLD AVG LOWRES: Bm_x,Bm_y,Bm_z')
+print(datetime.now())
 save_snapshot(PATH,Bm_x_lowres_MLD.isel(time=0),'Bm_x_lowres_MLD')
 save_snapshot(PATH,Bm_y_lowres_MLD.isel(time=0),'Bm_y_lowres_MLD')
 save_snapshot(PATH,Bm_z_lowres_MLD.isel(time=0),'Bm_z_lowres_MLD')
 
 
 print('SAVE DATA: INPUT, OUTPUT, LOSS')
-
+print(datetime.now())
 # Save input, output and loss data
 
 def save_data(PATH, dataset_ind, Um_lowres_MLD, Vm_lowres_MLD, Wm_lowres_MLD, Bm_lowres_MLD,UsBs_lowres_MLD, VsBs_lowres_MLD, WsBs_lowres_MLD, Bm_x_lowres_MLD, Bm_y_lowres_MLD, Bm_z_lowres_MLD):
@@ -454,25 +500,27 @@ train_ind, test_ind, val_ind =  rand_ind[:round(train_percent*time_ind)], rand_i
 
 # Train set
 print('TRAIN: START')
+print(datetime.now())
 TRAIN_PATH = BASE+CASE_NAME+'train/'
 save_data(TRAIN_PATH, train_ind, Um_lowres_MLD, Vm_lowres_MLD, Wm_lowres_MLD, Bm_lowres_MLD,UsBs_lowres_MLD, VsBs_lowres_MLD, WsBs_lowres_MLD, Bm_x_lowres_MLD, Bm_y_lowres_MLD, Bm_z_lowres_MLD)
 print('TRAIN: DONE')
 
 # TEST set
 print('TEST: START')
+print(datetime.now())
 TEST_PATH = BASE+CASE_NAME+'test/'
 save_data(TEST_PATH, test_ind,  Um_lowres_MLD, Vm_lowres_MLD, Wm_lowres_MLD, Bm_lowres_MLD,UsBs_lowres_MLD, VsBs_lowres_MLD, WsBs_lowres_MLD, Bm_x_lowres_MLD, Bm_y_lowres_MLD, Bm_z_lowres_MLD)
 print('TEST: DONE')
 
 # VAL set
 print('VAL: START')
+print(datetime.now())
 VAL_PATH = BASE+CASE_NAME+'val/'
 save_data(VAL_PATH, val_ind, Um_lowres_MLD, Vm_lowres_MLD, Wm_lowres_MLD, Bm_lowres_MLD,UsBs_lowres_MLD, VsBs_lowres_MLD, WsBs_lowres_MLD, Bm_x_lowres_MLD, Bm_y_lowres_MLD, Bm_z_lowres_MLD)
 print('VAL: DONE')
-
-
+'''
 print('COMPLETE PREPROCESS')
-
+print(datetime.now())
 
 
 
